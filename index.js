bbfddb63cb8b561964e3b5bc5bb2a06e20a479ab
minlaxz <minminlaxz@gmail.com>
2021-10-05 23:49:46 +0630

    Some

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.status(200).send('Hello World!'));
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(process.env.PORT || `3000`, () => console.log(`Server started on port: ${process.env.PORT}`));
